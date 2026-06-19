da1008c291023b094e2af8a4ff3f1240bccda9f5
Henry Udogu <chibuezeudogu8@gmail.com>
2026-06-19 15:16:35 -0700

    woo

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const activityRoutes = require('./routes/activities');
 const app = express();
 
 app.use(cors({
-  origin: ['https://https://gona-pi.vercel.app', 'http://localhost:5173'],
+  origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
